53b6fbbe55f33e014d5c8f983a07b28b1fb6d519
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-10-17 14:59:15 +0700

    Init skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='TeXid',
+    packages=['texid'],
+    version='0.1.0',
+    description='Tense Identification',
+    author='Le Minh Khoi',
+    license='MIT',
+)
